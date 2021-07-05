c88ffa7fa09a395e360d49307ecc104cb71909e2
jackjona123 <65679392+jackjona123@users.noreply.github.com>
2021-07-05 09:25:10 -0400

    production

---
+++ src/redux/store.js
@@ -0,0 +1,23 @@
+import { applyMiddleware, createStore } from "redux"
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from "redux-logger";
+import createSagaMiddleware from 'redux-saga';
+import { persistStore } from "redux-persist";
+import rootReducer from "./rootReducer";
+import { rootSaga } from "./rootSaga";
+
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [thunk, sagaMiddleware];
+
+
+// use 'development' to see console logs instead of 'production'
+if (process.env.NODE_ENV === 'production') {
+    middlewares.push(logger);
+}
+
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
+sagaMiddleware.run(rootSaga);
+export const persistor = persistStore(store);
+
+export default { store, persistor };
